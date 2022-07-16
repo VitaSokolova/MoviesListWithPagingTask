13fe36ec15ede8d5b6eec863b9e5d85a1d04d227
VitaSokolova <vitasokolovavsu@gmail.com>
2022-07-16 16:47:35 +0300

    Add tests, which should be implemented

---
+++ app/src/test/java/com/example/searchwithpaginationtask/presentation/MainViewModelTest.kt
@@ -0,0 +1,31 @@
+package com.example.searchwithpaginationtask.presentation
+
+import org.junit.Test
+
+internal class MainViewModelTest {
+
+    @Test
+    fun `when search query is empty should show empty state`() {
+
+    }
+
+    @Test
+    fun `when nothing is found should show try another query`() {
+
+    }
+
+    @Test
+    fun `when query is changed should show reload the data`() {
+
+    }
+
+    @Test
+    fun `when user scrolls to the bottom should request a new page`() {
+
+    }
+
+    @Test
+    fun `when clear query is clicked should show empty state`() {
+
+    }
+}
